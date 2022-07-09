2fc1b9494aae1706549bbeee953f978fe33f83c0
Vagif <vagif_m@bk.ru>
2022-07-10 01:19:49 +0300

    init commit

---
+++ docker-demo-project/src/main/java/mamedov/java/docker/DockerDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package mamedov.java.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemoProjectApplication.class, args);
+	}
+
+}
---
+++ docker-demo-project/src/test/java/mamedov/java/docker/DockerDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package mamedov.java.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
